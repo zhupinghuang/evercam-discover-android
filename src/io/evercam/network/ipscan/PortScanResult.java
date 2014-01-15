180840519cd3d3e254c9eb27b3888655207b99a0
Liuting Du <kangtadlt@gmail.com>
2014-01-15 09:56:05 +0000

    move scanning pieces into new library

--- src/io/evercam/network/ipscan/PortScanResult.java
+++
@@ -1,6 +0,0 @@
-package io.evercam.network.ipscan;
-
-public interface PortScanResult
-{
-	public void onPortActive(int port, int type);
-}
--- src/io/evercam/network/ipscan/ScanResult.java
+++
@@ -1,6 +0,0 @@
-package io.evercam.network.ipscan;
-
-public interface ScanResult
-{
-	public void onActiveIp(String ip);
-}
