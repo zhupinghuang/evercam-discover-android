b567bcb157233121d48077d119856cbafa6e23e5
Liuting Du <kangtadlt@gmail.com>
2014-01-15 09:56:05 +0000

    move scanning pieces into new library

--- src/io/evercam/network/upnp/UpnpResult.java
+++
@@ -1,8 +0,0 @@
-package io.evercam.network.upnp;
-
-import net.sbbi.upnp.devices.UPNPRootDevice;
-
-public interface UpnpResult
-{
-	public abstract void onUpnpDeviceFound(UPNPRootDevice upnpDevice);
-}
