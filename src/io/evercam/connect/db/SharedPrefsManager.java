f5ba37619bb6c51869ba4e2335806814dad179c8
Liuting Du <kangtadlt@gmail.com>
2014-01-24 13:07:32 +0000

    sign out

--- src/io/evercam/connect/db/SharedPrefsManager.java
+++ src/io/evercam/connect/db/SharedPrefsManager.java
@@ -7,25 +7,36 @@ public class SharedPrefsManager
 {
 	public static void clearGoogleUserInfo(SharedPreferences sharedPrefs)
 	{
-		SharedPreferences.Editor editor = sharedPrefs
-				.edit();
-		editor.putString(Constants.KEY_USER_EMAIL,
-				null);
-		editor.putString(
-				Constants.KEY_USER_FIRST_NAME, null);
-		editor.putString(
-				Constants.KEY_USER_LAST_NAME, null);
+		SharedPreferences.Editor editor = sharedPrefs.edit();
+		editor.putString(Constants.KEY_USER_EMAIL, null);
+		editor.putString(Constants.KEY_USER_FIRST_NAME, null);
+		editor.putString(Constants.KEY_USER_LAST_NAME, null);
 		editor.commit();
 	}
 	
-	public static void saveEvercamCredential(SharedPreferences sharedPrefs, String username, String password)
+	public static void clearEvercamUserInfo(SharedPreferences sharedPrefs)
+	{
+		SharedPreferences.Editor editor = sharedPrefs.edit();
+		editor.putString(Constants.EVERCAM_USERNAME, null);
+		editor.putString(Constants.EVERCAM_PASSWORD, null);
+		editor.commit();
+	}
+	
+	public static void clearAllUserInfo(SharedPreferences sharedPrefs)
+	{
+		clearGoogleUserInfo(sharedPrefs);
+		clearEvercamUserInfo(sharedPrefs);
+	}
+
+	public static void saveEvercamCredential(SharedPreferences sharedPrefs,
+			String username, String password)
 	{
 		SharedPreferences.Editor editor = sharedPrefs.edit();
 		editor.putString(Constants.EVERCAM_USERNAME, username);
 		editor.putString(Constants.EVERCAM_PASSWORD, password);
 		editor.commit();
 	}
-	
+
 	public static boolean isSignedWithGoogle(SharedPreferences sharedPrefs)
 	{
 		if (sharedPrefs.getString(Constants.KEY_USER_EMAIL, null) != null)
@@ -34,7 +45,7 @@ public class SharedPrefsManager
 		}
 		return false;
 	}
-	
+
 	public static boolean isSignedWithEvercam(SharedPreferences sharedPrefs)
 	{
 		if (sharedPrefs.getString(Constants.EVERCAM_USERNAME, null) != null)
@@ -43,13 +54,23 @@ public class SharedPrefsManager
 		}
 		return false;
 	}
-	
+
 	public static boolean isSigned(SharedPreferences sharedPrefs)
 	{
-		if(isSignedWithGoogle(sharedPrefs)||isSignedWithEvercam(sharedPrefs))
+		if (isSignedWithGoogle(sharedPrefs) || isSignedWithEvercam(sharedPrefs))
 		{
 			return true;
 		}
 		return false;
+	}
+	
+	public static String[] getGoogle(SharedPreferences sharedPrefs)
+	{
+		return new String[] {sharedPrefs.getString(Constants.KEY_USER_EMAIL, null),sharedPrefs.getString(Constants.KEY_USER_FIRST_NAME, null),sharedPrefs.getString(Constants.KEY_USER_LAST_NAME, null)};
+	}
+	
+	public static String[] getEvercam(SharedPreferences sharedPrefs)
+	{
+		return new String[] {sharedPrefs.getString(Constants.EVERCAM_USERNAME, null),sharedPrefs.getString(Constants.EVERCAM_PASSWORD, null)};
 	}
 }
