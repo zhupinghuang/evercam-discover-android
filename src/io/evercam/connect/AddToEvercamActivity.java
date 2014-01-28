2e8b412c13225de7551d61bfdffa5d347dcdea95
Liuting Du <kangtadlt@gmail.com>
2014-01-28 17:24:56 +0000

    create camera task, simulate network condition

--- src/io/evercam/connect/AddToEvercamActivity.java
+++ src/io/evercam/connect/AddToEvercamActivity.java
@@ -1,16 +1,25 @@
 package io.evercam.connect;
 
+import java.util.TimeZone;
+
+import io.evercam.CameraDetail;
+import io.evercam.EvercamException;
 import io.evercam.connect.db.Camera;
+import io.evercam.connect.db.SharedPrefsManager;
+import io.evercam.connect.net.NetInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 
 public class AddToEvercamActivity extends Activity
 {
@@ -25,7 +34,18 @@ public class AddToEvercamActivity extends Activity
 	private EditText vendorEdit;
 	private RadioButton publicRadioBtn;
 	private Button addBtn;
-	private CreateCameraTask addEvercamTask;
+	private CreateCameraTask createCameraTask;
+	private SharedPreferences sharedPrefs;
+	private String cameraId;
+	private String cameraName;
+	private String snapshotPath;
+	private boolean isPublic;
+	private int exthttp;
+	private String cameraUsername;
+	private String cameraPassword;
+	private String cameraModel;
+	private String cameraVendor;
+	private ProgressDialog progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -33,7 +53,7 @@ public class AddToEvercamActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_to_evercam);
 		
-		addEvercamTask = new CreateCameraTask();
+		sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		
 		camera = (Camera) getIntent().getSerializableExtra("camera");
 		initPage();
@@ -44,7 +64,15 @@ public class AddToEvercamActivity extends Activity
 			@Override
 			public void onClick(View v)
 			{
-				checkDetails();
+				if(detailsChecked())
+				{
+					if(createCameraTask != null)
+					{
+						createCameraTask = null;
+					}
+					createCameraTask = new CreateCameraTask();
+					createCameraTask.execute();
+				}
 			}			
 		});
 	}
@@ -69,25 +97,21 @@ public class AddToEvercamActivity extends Activity
 		snapshotEdit.setText(camera.getSnapshotJpgUrl());
 		usernameEdit.setText(camera.getUsername());
 		passwordEdit.setText(camera.getPassword());
-		modelEdit.setText(camera.getModel());
-		vendorEdit.setText(camera.getVendor());
-	}
-	
-	private class CreateCameraTask extends AsyncTask<Void,Void,Void>
-	{
-	
-		@Override
-		protected void onPreExecute()
+		if(camera.hasModel())
 		{
-			
+			if(camera.getModel().startsWith(camera.getVendor()))
+			{
+				camera.setModel(camera.getModel().substring(camera.getVendor().length()+1).trim());
+			}
+		modelEdit.setText(camera.getModel().toLowerCase());
 		}
-
-		@Override
-		protected Void doInBackground(Void... arg0)
+		if(camera.hasVendor())
 		{
-			return null;
+		vendorEdit.setText(camera.getVendor().toLowerCase());
 		}
-		
+		String[] evercamAccount = SharedPrefsManager.getEvercam(sharedPrefs);
+		idEdit.setText(evercamAccount[0] + camera.getMAC());
+		nameEdit.setText(R.string.myCamera);
 	}
 	
 	private void showErrorToast(int id)
@@ -95,7 +119,7 @@ public class AddToEvercamActivity extends Activity
 		Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
 	}
 	
-	private void checkDetails()
+	private boolean detailsChecked()
 	{
 		String idStr = idEdit.getText().toString();
 		String nameStr = nameEdit.getText().toString();
@@ -103,9 +127,24 @@ public class AddToEvercamActivity extends Activity
 		String exthttpStr = exthttpEdit.getText().toString();
 		String usernameStr = usernameEdit.getText().toString();
 		String passwordStr = passwordEdit.getText().toString();
-		String modelStr = modelEdit.getText().toString();
-		String vendorStr = vendorEdit.getText().toString();
-		Boolean isPublic = publicRadioBtn.isChecked();
+		
+		try
+		{
+			exthttp = Integer.parseInt(exthttpStr);
+			if (exthttp >= 0 && exthttp <= 65535)
+			{
+				return true;
+			}
+			else
+			{
+				showErrorToast(R.string.portRangeMsg);
+				return false;
+			}
+		}
+		catch (NumberFormatException e)
+		{
+			showErrorToast(R.string.portRangeMsg);
+		}
 		
 		if(idStr.length()== 0)
 		{
@@ -122,6 +161,10 @@ public class AddToEvercamActivity extends Activity
 			showErrorToast(R.string.snapshotEmpty);
 			snapshotEdit.requestFocus();
 		}
+		else if (!snapshotStr.startsWith("/"))
+		{
+			showErrorToast(R.string.snapshotInvalid);
+		}
 		else if(exthttpStr.length()== 0)
 		{
 			showErrorToast(R.string.exthttpEmpty);
@@ -136,6 +179,89 @@ public class AddToEvercamActivity extends Activity
 		{
 			showErrorToast(R.string.passwordEmpty);
 			passwordEdit.requestFocus();
+		}
+		else
+		{
+			cameraId = idStr;
+			cameraName = nameStr;
+			snapshotPath = snapshotStr;
+			isPublic = publicRadioBtn.isChecked();
+			cameraUsername = usernameStr;
+			cameraPassword = passwordStr;
+			cameraModel= modelEdit.getText().toString();
+			cameraVendor = vendorEdit.getText().toString();			
+			return true;
+		}
+		return false;
+	}
+	
+	private class CreateCameraTask extends AsyncTask<Void,Void,Void>
+	{
+		CameraDetail cameraDetail = new CameraDetail();
+	
+		@Override
+		protected void onPreExecute()
+		{
+			progressDialog = ProgressDialog.show(AddToEvercamActivity.this,
+					"", "Creating camera...", true);
+		}
+
+		@Override
+		protected void onPostExecute(Void result)
+		{
+			if (progressDialog.isShowing())
+			{
+				progressDialog.dismiss();
+			}
+		}
+
+
+		@Override
+		protected Void doInBackground(Void... arg0)
+		{
+//			initialDetailObject();
+//			try
+//			{
+//				io.evercam.Camera.create(cameraDetail);
+//				Log.v("evercamconnect", "success");
+//			}
+//			catch (EvercamException e)
+//			{
+//				e.printStackTrace();
+//				Log.v("evercamconnect", "failure");
+//			}
+			try
+			{
+				Thread.sleep(5000);
+			}
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		private void initialDetailObject()
+		{
+			String externalIp = NetInfo.getExternalIP();
+			String timezoneID = TimeZone.getDefault().getID();
+			Log.v("evercamconnect", timezoneID);
+			cameraDetail.setId(cameraId);
+			cameraDetail.setName(cameraName);
+			cameraDetail.setBasicAuth(cameraUsername, cameraPassword);
+			cameraDetail.setSnapshotJPG(snapshotPath);
+			cameraDetail.setPublic(isPublic);
+			cameraDetail.setEndpoints(new String[]{"http://" + externalIp});
+			cameraDetail.setTimezone(timezoneID);
+			if(!cameraVendor.equals("Unknown Vendor") && cameraVendor.length()!=0)
+			{
+				cameraDetail.setVendor(cameraVendor);
+			}
+			if(cameraModel.length()!=0)
+			{
+				cameraDetail.setModel(cameraModel);
+			}
 		}
 	}
 }
